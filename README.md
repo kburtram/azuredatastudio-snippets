8935cc7c8a33196ea6b668b7de60a8c8494a53db
Karl Burtram <karlb@microsoft.com>
2019-03-05 19:46:47 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Azure Data Studio Snippets
+This repo contains various code snippets that demonstrate various Azure Data Studio extensibility API usages.
---
+++ samples/queryplan/src/typings/ref.d.ts
@@ -0,0 +1,9 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+/// <reference path='../../../ref/vscode.d.ts'/>
+/// <reference path='../../../ref/azdata.d.ts'/>
+/// <reference path='../../../ref/azdata.proposed.d.ts'/>
+/// <reference types='@types/node'/>
